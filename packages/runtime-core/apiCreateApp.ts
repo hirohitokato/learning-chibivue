f2737355ba0f1ef6aae9b2147e6ce9cce073821f
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2025-12-06 20:57:58 +0900

    TYPO修正

--- packages/runtime-core/apiCreateApp.ts
+++ packages/runtime-core/apiCreateApp.ts
@@ -5,18 +5,19 @@ export interface App<HostElement = any> {
   mount: (rootContainer: HostElement | string) => void;
 }
 
-export type CreateAppFunciton<HostElement> = (
+export type CreateAppFunction<HostElement> = (
   rootComponent: Component
 ) => App<HostElement>;
 
 export function createAppAPI<HostElement>(
   render: RootRndererFunction<HostElement>
-): CreateAppFunciton<HostElement> {
+): CreateAppFunction<HostElement> {
   return function createApp(rootComponent: Component): App<HostElement> {
     const app: App = {
       mount: (rootContainer: HostElement) => {
-        const message = rootComponent.render!();
-        render(message, rootContainer);
+        const vnode = rootComponent.render!();
+        console.log("vnode:", vnode);
+        render(vnode, rootContainer);
       },
     };
     return app;
